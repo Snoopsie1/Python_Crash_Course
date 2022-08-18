cd6e68e843b065b3e9fad6c6293d2f49f691437f
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-18 20:34:05 +0200

    added chapter 1 and 2

---
+++ chapter_2_variables_and_simple_data_types/main.py
@@ -0,0 +1,13 @@
+#Variablesssss how do they work? What do they dooooooo, let's find out
+message = "Hello Python World"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+#Time to spoof an error
+#message = "Hello Python Crash Course - Repository reader:)"
+mesage = "Hello Python Crash Course - Repository reader:)"
+print(mesage)
+#Got the error "NameError: name 'mesage' is not defined.
+
