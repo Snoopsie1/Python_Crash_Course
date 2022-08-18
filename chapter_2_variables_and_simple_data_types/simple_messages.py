284d6c853d41a3658c8261907215a8f473107e37
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-18 20:52:48 +0200

    tasks 2.2 complete

---
+++ chapter_2_variables_and_simple_data_types/simple_messages.py
@@ -0,0 +1,7 @@
+#Assign a message to a variable, and print that message.
+#Then change the value of the variable to a new message, and print the new message.
+message = "Hello I am NOT changed"
+print(message)
+
+message = "Hello I AM changed"
+print(message)
