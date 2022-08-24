022228a35256192ebd0f4072dfd5068e42b0b86a
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:31:30 +0200

    underscores in numbers

---
+++ chapter_2_variables_and_simple_data_types/underscores_in_numbers.py
@@ -0,0 +1,6 @@
+#When you are writing long numbers,
+#you can group digits using underscores to make large numbers more readable:
+
+universe_age = 14_000_000_000
+#Then when it prints it, it removes the underscores, so it outputs the correct structure of data.
+print(universe_age)
