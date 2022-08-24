ff5d47eb9aaf1ad8017ab745847dd55d6341aadd
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:15:58 +0200

    task 2-6 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-6_Famous_Quote2.py
@@ -0,0 +1,9 @@
+#Repeat exercise 2-5, but this time,
+#represent the famous person's name using a variable called famous_person.
+#Then compose your message and represent it with a new variable called message.
+#Print your message
+#[DISCLAIMER] I sort of already did that in 2-5. But whatever, practice makes perfect.
+
+famous_person = "The Mother (From How I Met Your Mother)"
+message = f'{famous_person} once said, "Funny how sometimes you just... find things."'
+print(message)
