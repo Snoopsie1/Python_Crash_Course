524bdc082f4c54312c3014a5f8da334f0944ec19
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:08:34 +0200

    task 2_5 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-5_Famous_Quote.py
@@ -0,0 +1,8 @@
+#Find a quote from a famous person you admire.
+#Print the quote and the name of it's author.
+#Your output should look something like the following, including the quotation marks:
+# Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+person = "Bo Burnham"
+quote = "If you can live your life without an audience... You should do it."
+print(f'{person} once said, "{quote}"')
