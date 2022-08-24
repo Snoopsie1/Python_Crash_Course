2dce8f277b869ff4804a6d7c04a634002e278c2e
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:35:31 +0200

    constants and file name change

---
+++ chapter_2_variables_and_simple_data_types/constants.py
@@ -0,0 +1,6 @@
+#A constant is a variable whose value stays the same throughout the life of a program.
+#Python doesn't have built-in constant types.
+#But Python-Programmes use all capital letters to indicate a variable should be treated as a constant
+#and never be changed.
+
+MAX_CONNECTIONS = 5000
