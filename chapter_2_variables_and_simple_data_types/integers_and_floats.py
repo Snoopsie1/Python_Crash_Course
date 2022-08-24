3132aaf85a82befebed1daa70ba6ecda00ff3f04
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:30:00 +0200

    integers and floats

---
+++ chapter_2_variables_and_simple_data_types/integers_and_floats.py
@@ -0,0 +1,9 @@
+#When you divide any two numbers,
+# even if they are integers that result in a whole number, you'll always get a float:
+
+print(4 / 2)
+
+#If you mix an integer and a float in any other operation, you'll get a float as well:
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
