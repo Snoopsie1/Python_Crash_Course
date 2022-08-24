e4575bf7da6efe63fd307b200688351272d2c4c7
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:42:53 +0200

    task 2-8 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-8_Number_Eight.py
@@ -0,0 +1,15 @@
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+#Be sure to enclose your operations in print() calls to see the results.
+#You should create a line for each operation:
+
+#Addition
+print(5+3)
+
+#Subtraction
+print(12-4)
+
+#Multiplication
+print(2 * 4)
+
+#Division
+print(16 / 2)
