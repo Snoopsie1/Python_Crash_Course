083e50cae34db2afdda7ed1a18ab60169cc62c2b
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:25:23 +0200

    integers

---
+++ chapter_2_variables_and_simple_data_types/integers.py
@@ -0,0 +1,16 @@
+print("----- Simple Number Stuff -----")
+print(2+5)
+print(3-2)
+print(2*3)
+print(3/2)
+
+#To use exponents (Engelsk for "I anden/tredje/osv")
+print("----- Exponent Stuff -----")
+print(3**2)
+print(3**3)
+print(10**6)
+
+#Python supports order of operations too
+print("----- Order of Operations Stuff -----")
+print(2 + 3 * 4)
+print((2 + 3) * 4)
