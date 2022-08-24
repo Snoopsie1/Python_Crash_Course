00b4393c0dc23e826b9267bb431d8cd3442949b5
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:01:20 +0200

    task 2_3 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-3_Personal_Message.py
@@ -0,0 +1,8 @@
+#Use a variable to represent a person's name and print a message to that person.
+#Your message should be simple, such as, "Hello Eric, would you like to learn some Python today?"
+
+persons_fname = "Egner"
+persons_lname = "Johansen"
+message = f"Hello {persons_fname} {persons_lname}, would you like to learn some Python today?"
+print(message)
+
