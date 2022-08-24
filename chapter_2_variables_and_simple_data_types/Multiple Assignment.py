ea463aa07b14c7ec50f3b71ef80195ca9aab270b
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:33:23 +0200

    Multiple Assignments

---
+++ chapter_2_variables_and_simple_data_types/Multiple Assignment.py
@@ -0,0 +1,7 @@
+#You can assign values to more than one variable using just a single line.
+#This can help shorten your programs and make them easier to read;
+#You'll use this technique most often qhen initializing a set of numbers.
+#Like this:
+
+x, y, z = 0, 0, 0
+print(x,y,z)
