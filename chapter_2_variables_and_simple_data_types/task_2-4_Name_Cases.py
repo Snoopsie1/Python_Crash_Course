82356ee93ca5cf23d8a0cb0e7f6b1d0703b53237
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:03:40 +0200

    task 2_4 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-4_Name_Cases.py
@@ -0,0 +1,10 @@
+#Use a variable to represent a person's name,
+#and then print that person's name in lowercase, uppercase and title case
+
+name = "eigil mortensen"
+lowercase_name= name.lower()
+print(lowercase_name)
+uppercase_name = name.upper()
+print(uppercase_name)
+titlecase_name = name.title()
+print(titlecase_name)
