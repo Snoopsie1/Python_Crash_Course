038d4b48c9a789035fba63e19e819cb390687e75
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-24 13:44:26 +0200

    task 2-9 complete

---
+++ chapter_2_variables_and_simple_data_types/task_2-9_Favorite_Number.py
@@ -0,0 +1,6 @@
+#Use a variable to represent your favorite number.
+#Then, using that variable, create a message that reveals your favorite number.
+#Print that message.
+
+favorite_number = 14
+print(f"And... my favorite number is *Drum roll* .. {favorite_number}")
