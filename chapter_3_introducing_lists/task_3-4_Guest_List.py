d021ec37ebd3f76342066683ac3423bad3b68f3a
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-25 11:42:04 +0200

    task 3-4 complete

---
+++ chapter_3_introducing_lists/task_3-4_Guest_List.py
@@ -0,0 +1,14 @@
+#If you could invite anyone, living or deceased, to dinner,
+#who would you invite?
+#Make a list that includes at least three people you'd like to invite to dinner.
+#Then use your list to print a message to each person, inviting them to dinner.
+
+guest_list = ["hans michaelsen", "pasha rasoli", "bo burnham", "michael waddell"]
+print(f"Dear {guest_list[0].title()}, long time no see. You always asked me what I wanted to be when I grew up."
+      f" But I could never answer you when I was younger. "
+      f"Now that you aren't around anymore it would be nice to see you one last time to tell you that I am following your footsteps.")
+print(f"Dear {guest_list[1].title()}, how are you? I never got to meet you, so I hope you'd show up for a quick bite and a chat."
+      f"My parents say my personality is a lot like yours. Would love to finally meet you")
+print(f"Dear {guest_list[2].title()}, you are quite the funny one. Show up for some cake and deep talks")
+print(f"Dear {guest_list[3].title()}, I've never been more hookd on a card game before until your creation showed up in our shops."
+      f"Swing by for a bite and a quick chat. Would love to hear what the future of your product entails")
