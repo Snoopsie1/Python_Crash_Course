abb7bde027c93ed303267fe0b633d3779368ad4c
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-25 10:57:02 +0200

    task_3-2 complete

---
+++ chapter_3_introducing_lists/task_3-2_Greetings.py
@@ -0,0 +1,10 @@
+#Start with the list you used in Exercise 3-1,
+#but instead of just printing each person's name, print a message to them.
+#The text of each message should be the same, but each message should be personalized with the person's name.
+my_friends = ["Asta", "Cleve", "Bean", "Weinell", "Galler", "Michelle"]
+print(f"{my_friends[0]} is my beautiful soul-mate.")
+print(f"My day-to-day wouldn't have been the same without my right-hand man, {my_friends[1]}.")
+print(f"A day with {my_friends[2]}, brightens your mood.")
+print(f"My screen is less black and white in League of Legends, with {my_friends[3]} on my team.")
+print(f"Smash Bros sparring matches are always entertaining with {my_friends[4]}.")
+print(f"{my_friends[-1]}'s exclamations in League of Legends, is on a league of it's own")
