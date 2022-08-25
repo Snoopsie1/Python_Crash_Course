770a0116fc3209d882b3b5ee903cd28b0700bc0d
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-25 10:50:36 +0200

    task 3-1 complete

---
+++ chapter_3_introducing_lists/task_3-1_Names.py
@@ -0,0 +1,9 @@
+#Store the names of a few of your friends in a list called names.
+#Print each person's name by accessing each element in the list, one at a time.
+my_friends = ["Asta", "Cleve", "Bean", "Weinell", "Galler", "Michelle"]
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
+print(my_friends[3])
+print(my_friends[4])
+print(my_friends[5])
