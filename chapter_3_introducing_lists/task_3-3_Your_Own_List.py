73da2c63466e521235c2bf1a84144efb8d2ba944
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-25 11:03:25 +0200

    task_3-3 complete

---
+++ chapter_3_introducing_lists/task_3-3_Your_Own_List.py
@@ -0,0 +1,11 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car.
+#And make a list that stores several examples.
+#Use your list to print a series of statements about these items,
+#such as "I would like to own a Honda motorcycle"
+
+transportation_favorites = ["car", "bike", "longboard", "paddle-board", "train"]
+print(f"Nothing makes a breezy summer evening than a trip on your summer {transportation_favorites[1]}.")
+print(f'When I get a job, and start to make some "adult money", then I would like to buy a Tesla {transportation_favorites[0]}.')
+print(f"I remember I would spent most of my teenage years on clean asphalt cruising on my {transportation_favorites[2]}.")
+print(f"Last summer I was lucky enough to get a {transportation_favorites[3]}, I've been out paddling a few times.")
+print(f"Now that I've moved into the capital. day-to-day travel is a lot easier with {transportation_favorites[-1]}s.")
