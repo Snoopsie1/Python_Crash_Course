73cb3221d846f4f54ef47c3d2bced2a7e9253868
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-27 20:23:08 +0200

    task 3-9 complete

---
+++ chapter_3_introducing_lists/task_3-9_Dinner_Guests.py
@@ -0,0 +1,4 @@
+#Working with one of the programs from Exercises 3-4 through 3-7,
+# use len() to print a message indicating the number of people you are inviting to dinner.
+guest_list = ["hans michaelsen", "pasha rasoli", "bo burnham", "michael waddell"]
+print(f"The amount of guests I've invited is: {len(guest_list)}. Bon appetite")
