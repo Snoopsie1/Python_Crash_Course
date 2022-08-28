796f7edc7dfd86164b07d3ea0165fa836a32bb19
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-28 21:42:48 +0200

    tuples

---
+++ chapter_4_(loops)_working_with_lists/tuples.py
@@ -0,0 +1,21 @@
+#List of items that cannot change are called Tuples.
+# They are what you call for "Immutable Lists"
+# Bon appétit.
+
+#A Tuple looks just like a list except you use a parentheses
+# instead of square brackets when defining them.
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+#You can access data in a tuple just like you can in a normal list.
+
+#dimensions[0] = 250
+#That line of code will raise an error, because you cannot change what is on the tuple.
+
+#Sidenote about Tuple:
+# Tuples are technically defined by the presence of a comma;
+# the parentheses make them look neater and more readable
+# If you want to define a tuple with one element, you need to include a trailing comman:
+my_t = (3,)
+#It doesn't often make sense to build a tuple with one element,
+# but this can happen when tuples are generated automatically
