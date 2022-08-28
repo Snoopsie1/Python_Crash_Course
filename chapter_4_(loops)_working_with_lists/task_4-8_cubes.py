ba00407eb570d4f507e2e39f738e01ca32e01d84
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-28 20:30:05 +0200

    task 4-8 update

--- chapter_4_(loops)_working_with_lists/task_4-8_cubes.py
+++ chapter_4_(loops)_working_with_lists/task_4-8_cubes.py
@@ -3,7 +3,7 @@
 # Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10),
 # and use a for loop to print out the value of each cube.
 
-cubes = [value**3 for value in range(1, 11, 1)]
+cubes = [value**3 for value in range(1, 11)]
 for cube in cubes:
     print(cube)
 
