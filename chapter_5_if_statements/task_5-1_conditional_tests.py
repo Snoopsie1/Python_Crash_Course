e6485d7ba01f98314d9b01d309f6d622f1a4be7c
Snoopsie1 <oliver.rasoli@gmail.com>
2022-08-28 23:31:56 +0200

    task 5-1 done

---
+++ chapter_5_if_statements/task_5-1_conditional_tests.py
@@ -0,0 +1,21 @@
+#Write a series of condtional tests.
+# Print a statement describing each test and your prediction for the results of each test.
+car = "subaru"
+print("Is car == 'subaru'? I predict True")
+print(car == "subaru")
+
+print("\nIs car == 'audi'? I predict False")
+print(car == "audi")
+# - Create a few more tests.
+edible = "apple"
+print("\nIs edible == 'grass'? I predict False")
+print(edible == "grass")
+print("Is edible == 'chair'? I predict False")
+print(edible == "chair")
+print("Is edible == 'cloth'? I predict False")
+print(edible == "cloth")
+print("Is edible == 'computer'? I predict False")
+print(edible == "computer")
+print("Is edible == 'apple'? I predict True")
+print(edible == "apple")
+
