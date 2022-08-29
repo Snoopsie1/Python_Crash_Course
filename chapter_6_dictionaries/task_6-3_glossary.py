a5a82591930593354c9a02a06105f84665c885ac
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-08-29 13:30:29 +0200

    task 6-3 done

---
+++ chapter_6_dictionaries/task_6-3_glossary.py
@@ -0,0 +1,22 @@
+#A Python dictionary can be used to model an actual dictionary.
+# However, to avoid confusion, let's call it a glossary.
+# · Think of five programming words you've learned about in the previous chapters
+#   Use these words as the keys in your glossary, and store their meanings as values.
+glossary = {
+    "loop": "automate a repetitive task",
+    "list": "store multiple things",
+    "elif": "beautiful conditional chains/blocks",
+    "pop()": "removing and using said removed item",
+    "slicing": "taking part of a list, or copying an entire list with slicing",
+    "every-other slicing": "the weird syntax for printing every-other thing on a sliced-list: list[::2]"
+    }
+# · Print each word and its meaning as neatly formatted output.
+#   You might print the word followed by a colon and then its meaning,
+#   or print the word on one line and then print its meaning indented on a second line.
+#   Use the newline (\n) to insert a blank line between each word-meaning pair in your output
+print(f"Loops:\n\t{glossary['loop'].title()}")
+print(f"Lists:\n\t{glossary['list'].title()}")
+print(f"Elif:\n\t{glossary['elif'].title()}")
+print(f"pop():\n\t{glossary['pop()'].title()}")
+print(f"Slicing:\n\t{glossary['slicing'].title()}")
+print(f"Every-Other Slicing:\n\t{glossary['every-other slicing'].title()}")
