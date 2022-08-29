e94d4d4b45e2e9892c4966059e29f629ae3924a1
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-08-29 13:23:19 +0200

    task 6-2 done

---
+++ chapter_6_dictionaries/task_6-2_favorite_numbers.py
@@ -0,0 +1,16 @@
+#Use a dictionary to store people's favorite numbers.
+# Think of five names, and use them as keys in your dictionary.
+# Think of a favorite number for each person, and store each as a value in your dictionary.
+# Print each person's name and their favorite number
+persons_fave_number = {
+    "gunner": 42,
+    "eigil": 61,
+    "oliver": 14,
+    "max": 9,
+    "asta": 7,
+}
+print(f"Gunner's favorite number is: {persons_fave_number['gunner']}")
+print(f"Eigil's favorite number is: {persons_fave_number['eigil']}")
+print(f"Oliver's favorite number is: {persons_fave_number['oliver']}")
+print(f"Max's favorite number is: {persons_fave_number['max']}")
+print(f"Asta's favorite number is: {persons_fave_number['asta']}")
