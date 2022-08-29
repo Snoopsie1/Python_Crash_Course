bad867c29144c2dc5330e73913fa2c006b2c719e
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-08-29 12:18:19 +0200

    empty dictionaries

---
+++ chapter_6_dictionaries/empty_dictionary.py
@@ -0,0 +1,10 @@
+#Sometimes it's convenient to start with a clean, empty dictionary and fill it up as the program goes.
+# Like an inventory in a game perhaps?
+player_inventory = {}
+print(player_inventory)
+
+player_inventory["weapon"] = "hands of gu'den"
+player_inventory["armor"] = "vestments of jörg"
+print(player_inventory)
+
+#Typically you'll use empty dictionaries when storing user-supplied data.
