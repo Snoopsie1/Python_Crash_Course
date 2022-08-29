8b72a2f7682c5a3415020162c0e578b878df884c
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-08-29 12:14:48 +0200

    adding new key-value pairs

---
+++ chapter_6_dictionaries/adding_new_key-value_pairs.py
@@ -0,0 +1,10 @@
+#You can also just add new key-value pairs to a dictionary:
+# Dictionaries are a dynamic structure, like a list.
+alien_0 = {"color": "green", "points": 5}
+print(alien_0)
+
+alien_0["x-position"] = 0
+alien_0["y-position"] = 25
+print(alien_0)
+
+#These few lines added x-position-0 and y-position-25 to the dictionary!
