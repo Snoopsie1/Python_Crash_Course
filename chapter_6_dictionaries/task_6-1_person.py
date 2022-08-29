526f7fe0025a8781849f26c76f7a6bb8d9212a4f
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-08-29 13:19:13 +0200

    task 6-1 done

---
+++ chapter_6_dictionaries/task_6-1_person.py
@@ -0,0 +1,15 @@
+#Use a dictionary to store information about a person you know.
+# Store their first name, last name, age and the city in which they live.
+# You should have keys such as:
+# first_name, last_name, age and city
+# Print each piece of information stored in your dictionary.
+person_i_know = {
+    "first_name": "nikolaj",
+    "last_name": "osv",
+    "age": 44,
+    "city": "lyngby",
+    }
+print(f"{person_i_know['first_name'].title()}")
+print(f"{person_i_know['last_name'].title()}")
+print(f"{person_i_know['age']}")
+print(f"{person_i_know['city'].title()}")
